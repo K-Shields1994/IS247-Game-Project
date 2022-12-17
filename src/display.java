135c2d28b9012b147172f7b7ce91d00ffe44301d
K-Shields1994 <120108762+K-Shields1994@users.noreply.github.com>
2022-12-17 18:00:35 -0500

    Added display.java

---
+++ src/display.java
@@ -0,0 +1,6 @@
+public class display {
+    String line1 = new String(new char[60]).replace('\0', '-');
+    String line2 = new String(new char[35]).replace('\0', '#');
+    String line3 = new String(new char[14]).replace('\0', '*');
+    String line4 = new String(new char[19]).replace('\0', '*');
+}
