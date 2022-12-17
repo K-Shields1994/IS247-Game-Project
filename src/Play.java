5e508adc4697aed6f70125a842d521b1c5d6f883
K-Shields1994 <120108762+K-Shields1994@users.noreply.github.com>
2022-12-17 17:58:42 -0500

    Initial commit

---
+++ src/Play.java
@@ -0,0 +1,14 @@
+//Kevin Shields, Andrew Stine, Rabindra Singh
+
+//The code is found in the Game class. The Main calls everything from that class.
+
+
+public class Play {
+
+    public static void main(String[] args) {
+
+        Game g = new Game();
+        g.game();
+
+    }
+}
---
+++ src/classSelector.java
@@ -0,0 +1,9 @@
+public class classSelector {
+    public String classCharacter;
+    public int HP;
+
+    classSelector(String myClassSelector, int myHP) {
+        this.classCharacter = myClassSelector;
+        this.HP = myHP;
+    }
+}
